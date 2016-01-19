e633203e2fc0597e0c371c9d2a1d2a4df979cfdd
Yi-Ling Chen <puyenchild@gmail.com>
2016-01-19 14:05:30 +0800

    error handling

--- scripts/cropping_browser.py
+++ scripts/cropping_browser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import cv2
+import numpy as np
 from random import randint, shuffle
 from clint.textui import puts, colored
 try:
@@ -9,6 +10,40 @@ except:
     import pickle
 
 image_root = '../images/'
+
+def generate_comparison(image, x, y, w, h):
+    height, width, channel = image.shape
+    print height
+    print x, y, w, h
+
+    if x < 0:
+        x = 0
+    if y < 0:
+        y = 0
+
+    scale_o = float(h) / height    # scaling factor of the original image
+    new_width = int(width * (float(h) / height))
+
+    total_width = new_width + w + 100
+    total_height = h
+
+    crop = image[y:y+h, x:x+w, :]
+    print 'crop:', crop.shape
+    canvas = np.ones([total_height, total_width, 3], dtype=np.uint8) * 255
+    print 'canvas:', canvas.shape
+    res = cv2.resize(img, (new_width, h), interpolation=cv2.INTER_CUBIC)
+    #print res.shape
+    canvas[:, :res.shape[1], :] = res
+    canvas[:crop.shape[0], res.shape[1]+100:res.shape[1]+100+w, :] = crop
+
+    if total_width > total_height:
+        scale = 1400.0 / total_width
+        res = cv2.resize(canvas, (0,0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
+        cv2.imshow('Comparison', res)
+    else:
+        scale = 1000.0 / total_height
+        res = cv2.resize(canvas, (0,0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
+        cv2.imshow('Comparison', res)
 
 if __name__ == '__main__':
 
@@ -22,14 +57,16 @@ if __name__ == '__main__':
         filename = os.path.split(url)[-1]
         path = image_root + filename
         img = cv2.imread(path, cv2.IMREAD_COLOR)
-        cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
-        cv2.putText(img, username, (10, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2, cv2.CV_AA)
 
         # Error handling
         if img is None:
             puts(colored.red('ERROR: loading image failed!'))
             continue
 
+        generate_comparison(image=img, x=x, y=y, w=w, h=h)
+
+        cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
+        cv2.putText(img, username, (10, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2, cv2.CV_AA)
         cv2.imshow('Cropping', img)
         key = cv2.waitKey(0)
 
