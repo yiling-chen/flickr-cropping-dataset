f6e2e977c5b0677ef528e02b6a1f923ed99acf34
Yi-Ling Chen <puyenchild@gmail.com>
2016-01-22 15:11:40 +0800

    filter by username

--- scripts/cropping_browser.py
+++ scripts/cropping_browser.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
 import cv2
+import sys
+import argparse
 import numpy as np
 from random import randint, shuffle
 from clint.textui import puts, colored
@@ -13,14 +15,8 @@ image_root = '../images/'
 
 def generate_comparison(image, x, y, w, h):
     height, width, channel = image.shape
-    print height
-    print x, y, w, h
-    '''
-    if x < 0:
-        x = 0
-    if y < 0:
-        y = 0
-    '''
+    #print height
+    #print x, y, w, h
     scale_o = float(h) / height    # scaling factor of the original image
     new_width = int(width * (float(h) / height))
 
@@ -28,18 +24,18 @@ def generate_comparison(image, x, y, w, h):
     total_height = h
 
     crop = image[y:y+h, x:x+w, :]
-    print 'crop:', crop.shape
+    #print 'crop:', crop.shape
     canvas = np.ones([total_height, total_width, 3], dtype=np.uint8) * 255
-    print 'canvas:', canvas.shape
+    #print 'canvas:', canvas.shape
     res = cv2.resize(img, (new_width, h), interpolation=cv2.INTER_CUBIC)
-    print 'res:', res.shape
+    #print 'res:', res.shape
 
-    if randint(0,1) == 1:
-        canvas[:res.shape[0], :res.shape[1], :] = res
-        canvas[:crop.shape[0], res.shape[1]+100:res.shape[1]+100+crop.shape[1], :] = crop
-    else:
-        canvas[:crop.shape[0], :crop.shape[1], :] = crop
-        canvas[:res.shape[0], crop.shape[1]+100:crop.shape[1]+100+res.shape[1], :] = res
+    #if randint(0,1) == 1:
+    canvas[:res.shape[0], :res.shape[1], :] = res
+    canvas[:crop.shape[0], res.shape[1]+100:res.shape[1]+100+crop.shape[1] , :] = crop
+    #else:
+    #    canvas[:crop.shape[0], :crop.shape[1], :] = crop
+    #    canvas[:res.shape[0], crop.shape[1]+100:crop.shape[1]+100+res.shape[1], :] = res
 
     if total_width > total_height:
         scale = 1400.0 / total_width
@@ -50,15 +46,47 @@ def generate_comparison(image, x, y, w, h):
         res = cv2.resize(canvas, (0,0), fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
         cv2.imshow('Comparison', res)
 
+def lookup_users():
+    username_pool = set()
+    database = pickle.load( open('../cropping_results.pkl', 'rb') )
+    for photo_id, url, username, x, y, w, h in database:
+        if username not in username_pool:
+            username_pool.add(username)
+
+    for username in username_pool:
+        print username
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='A simple viewer for the photo cropping dataset.')
+    parser.add_argument(
+        '-u', '--username', type=str, default="Any",
+        help="Show the image cropped by the specified user only [default=Any]."
+    )
+    parser.add_argument(
+        '-l', '--list', type=bool, default=False,
+        help="List all the usernames [default=False]."
+    )
+    args = parser.parse_args()
+    uname = args.username
+    list_users = args.list
+    #print uname
+
+    if list_users:
+        lookup_users()
+        sys.exit()
+
 
     # load database
-    puts(colored.green("Loading exsiting database..."))
+    puts(colored.green("Loading database..."))
     database = pickle.load( open('../cropping_results.pkl', 'rb') )
     print "Current records: ", len(database)
     print
 
     for photo_id, url, username, x, y, w, h in database:
+        if uname != 'Any' and username != uname:
+            continue
+
         filename = os.path.split(url)[-1]
         path = image_root + filename
         img = cv2.imread(path, cv2.IMREAD_COLOR)
