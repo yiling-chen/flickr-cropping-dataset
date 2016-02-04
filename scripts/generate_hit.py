87b8da318562287ba08d2b6fa480f49d9ee5e4cc
Yi-Ling Chen <puyenchild@gmail.com>
2016-02-04 10:38:05 +0800

    push HIT from database

--- scripts/generate_hit.py
+++ scripts/generate_hit.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 """
 generate_hit.py
+
 Description:    A command-line interface for generating HITs to MTurk.
                 Usage is as follows:
                 $ python generate_hit.py -h
@@ -19,7 +20,8 @@ Description:    A command-line interface for generating HITs to MTurk.
                                     if nothing is provided
                   --secret SECRET   Your AWS secret access key. Will look for boto
                                     configuration file if nothing is provided
-Created by  (drew.conway@nyu.edu) on
+
+Created by  (yiling.chen.ntu@gmail.com) on
 # Copyright (c) , under the Simplified BSD License.
 # For more information on FreeBSD see: http://www.opensource.org/licenses/bsd-license.php
 # All rights reserved.
@@ -27,16 +29,66 @@ Created by  (drew.conway@nyu.edu) on
 from boto.mturk.price import Price
 from boto.mturk.connection import MTurkConnection
 from boto.mturk.qualification import Qualifications, Requirement
-from boto.mturk.question import Question,QuestionForm,QuestionContent,SelectionAnswer,AnswerSpecification
+from boto.mturk.question import Question,QuestionForm,QuestionContent,SelectionAnswer,AnswerSpecification,FormattedContent,Overview
 from generate_qualification import PhotoQualityQualificationTest,PhotoQualityQualificationType
 from clint.textui import puts, colored
 from datetime import datetime
+import cPickle as pickle
 import argparse
+
+def create_hit(mturk, URLs, num_assignment, qualification=Qualifications()):
+    # Constant data for HIT generation
+    hit_title = "Photo Quality Assessment"
+    hit_description = "This task involves viewing pairs of pictures and judging which picture among the image pair is more beautiful."
+    #base_reward = 0.05
+    lifetime = 259200
+    keywords = ["photo","quality","ranking"]
+    duration = 30 * 60
+    reward = 0.03
+    #approval_delay = 86400
+
+    # Question form for the HIT
+    question_form = QuestionForm()
+
+    overview = Overview()
+    overview.append_field('Title', 'Photo Quality Assessment')
+    overview.append(FormattedContent('For each question, please choose either the left or right image which you think is more beautiful in terms of its <u>composition</u>.'))
+    overview.append(FormattedContent('<b>Hints: Please make your decision based on several "rules of thumb" in photography, such as rule of thirds, visual balance and golden ratio.</b>'))
+    overview.append(FormattedContent('<b>You may also make your decision by judging which image contains less unimportant or distracting contents</b>.'))
+    question_form.append(overview)
+
+    ratings = [('Left', '0'), ('Right','1')]
+    for i in xrange(len(URLs)):
+        qc = QuestionContent()
+        qc.append_field('Title', 'Question')
+        qc.append_field('Text', 'Please indicate which one of the following images is more beautiful.')
+        qc.append(FormattedContent('<img src="'+URLs[i]+'" alt="Image not shown correctly!"></img>'))
+        URLs[i]
+        fta = SelectionAnswer(min=1, max=1, style='radiobutton', selections=ratings, type='text', other=False)
+        q = Question(identifier='photo_pair_'+str(i),
+                    content=qc,
+                    answer_spec=AnswerSpecification(fta),
+                    is_required=True
+        )
+        question_form.append(q)
+
+    hit_res = mturk.create_hit(title=hit_title,
+                                description=hit_description,
+                                reward=Price(amount=reward),
+                                duration=duration,
+                                keywords=keywords,
+                                #approval_delay=approval_delay,
+                                question=question_form,
+                                #lifetime=lifetime,
+                                max_assignments=num_assignment,
+                                qualifications=qualification)
+    # return HIT ID
+    return hit_res[0].HITId
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Command line interface for generating HITs to MTurk.')
-    parser.add_argument('--hits', metavar='N', type=int, default=10, help="The number of HITs to push. Default is 10.")
-    parser.add_argument('--assign', metavar='N', type=int, default=10, help="The number of assignments for each HIT. Default is 7.")
+    parser.add_argument('--hits', metavar='N', type=int, default=5, help="The number of HITs to push. Default is 10.")
+    parser.add_argument('--assign', metavar='N', type=int, default=1, help="The number of assignments for each HIT. Default is 7.")
     parser.add_argument("--prod", type=str, choices="yn", default="n",
         help="Should this HIT be pushed to the production server? Default is 'n'.")
 
@@ -98,51 +150,39 @@ if __name__ == '__main__':
     # Register test as a requirement for hit
     req = Requirement(qualification_type_id=qual_id,
                       comparator="GreaterThan",
-                      integer_value=80)
+                      integer_value=90)
 
     # Add qualification test
     qual = Qualifications()
     qual.add(req)
 
-    # Constant data for HIT generation
-    hit_title = "Photo Quality Assessment"
-    hit_description = "This task involves viewing pairs of pictures and judging which picture among the image pair is more beautiful."
-    base_reward = 0.05
-    lifetime = 259200
-    keywords = ["photo","quality","ranking"]
-    duration = 30 * 60
-    reward = 0.03
-    approval_delay = 86400
+    num_images_per_hit = 2
+    mturk_crop_db = pickle.load( open('../mturk_crop_db.pkl', 'rb') )
 
-    # Question form for the HIT
-    question_form = QuestionForm()
-    ratings = [('Left', '0'), ('Right','1')]
-    qc9 = QuestionContent()
-    qc9.append_field('Title', 'Question:')
-    qc9.append_field('Title', 'Indicate which one of the following images is more beautiful.')
-    fta9 = SelectionAnswer(min=1, max=1, style='radiobutton',
-                selections=ratings,
-                type='text',
-                other=False)
-    q9 = Question(identifier='raw_photo4',
-                content=qc9,
-                answer_spec=AnswerSpecification(fta9),
-                is_required=True)
+    indexes = []
+    for i in xrange(len(mturk_crop_db)):
+        if mturk_crop_db[i]['hit_id'] == 'n/a' and mturk_crop_db[i]['disabled'] == False:
+            indexes.append(i)
+        if len(indexes) == num_hits * num_images_per_hit:
+            break
 
-    question_form.append(q9)
+    #print indexes
+    import math
+    num_hits = int( math.ceil(len(indexes) / float(num_images_per_hit)) )
 
     # Create HITs
-    for j in xrange(num_hits):
-        print 'Pushing ', j, ' HIT...'
-        hit_res = mturk.create_hit(title=hit_title,
-                    description=hit_description,
-                    reward=reward,
-                    duration=duration,
-                    keywords=keywords,
-                    approval_delay=approval_delay,
-                    question=question_form,
-                    #lifetime=lifetime,
-                    max_assignments=num_assignment,
-                    qualifications=qual)
-        #hit_results.extend(hit_res)
+    for i in xrange(num_hits):
+        print 'Pushing HIT #', i, '...'
+        image_indexes = indexes[i*num_images_per_hit:(i+1)*num_images_per_hit]
+        print image_indexes
+        URLs = [mturk_crop_db[j]['url'] for j in image_indexes]
+        #print URLs
+        HITId = create_hit(mturk, URLs, num_assignment, qual)
+        puts(colored.green('\tID: {}'.format(HITId)))
 
+        # save results
+        for idx in xrange(len(image_indexes)):
+            mturk_crop_db[image_indexes[idx]]['hit_id'] = HITId
+            mturk_crop_db[image_indexes[idx]]['question_idx'] = idx
+
+    pickle.dump( mturk_crop_db, open('../mturk_crop_db.pkl', 'wb') )
